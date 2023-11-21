c85c6e57ef8bdc9a3fa58f9f58d71cbfc7a73969
Julianlee861112 <lee.yu-y@northeastern.edu>
2023-11-20 18:29:15 -0800

    Fixed typo

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const COURSES_URL = "https://kanbas-node-server-app-tq3j.onrender.com/api/courses";
-const MODULES_URL = "hhttps://kanbas-node-server-app-tq3j.onrender.com/api/modules";
+const MODULES_URL = "https://kanbas-node-server-app-tq3j.onrender.com/api/modules";
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const MODULES_URL = `${API_BASE}/modules`;
 // const COURSES_URL = `${API_BASE}/courses`;
