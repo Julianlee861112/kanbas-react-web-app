fe7eb984b8f8ceb06e79736a1ad7a85393b6082e
Julianlee861112 <lee.yu-y@northeastern.edu>
2023-11-20 18:22:11 -0800

    Changed URL

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-// const COURSES_URL = "https://kanbas-node-server-app-tq3j.onrender.com/api/courses";
-// const MODULES_URL = "hhttps://kanbas-node-server-app-tq3j.onrender.com/api/modules";
-const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
-const COURSES_URL = `${API_BASE}/courses`;
+const COURSES_URL = "https://kanbas-node-server-app-tq3j.onrender.com/api/courses";
+const MODULES_URL = "hhttps://kanbas-node-server-app-tq3j.onrender.com/api/modules";
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const MODULES_URL = `${API_BASE}/modules`;
+// const COURSES_URL = `${API_BASE}/courses`;
 export const deleteModule = async (moduleId) => {
   const response = await axios
     .delete(`${MODULES_URL}/${moduleId}`);
